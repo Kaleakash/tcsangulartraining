6c64dc2e26bc6f610b1edd3d78959aa16ca52d06
Akash <akash300383@gmail.com>
2020-11-05 22:16:50 +0530

    Fake Service Example

--- AngularProjects/angularformsandservice/src/app/fake/fake.component.ts
+++ AngularProjects/angularformsandservice/src/app/fake/fake.component.ts
@@ -12,11 +12,34 @@ export class FakeComponent implements OnInit {
 
   fakeObject:Fake[];    //created array object to load the data. 
 
+  employeeDetails:Employee[]=[];
+
   constructor(private fakeSer2:FakeService2) { }    // Pull object or DI
 
   ngOnInit(): void {
-    this.fakeSer2.loadData().subscribe(data=>this.fakeObject=data); //data store in fakeObject 
+/*    this.fakeSer2.loadData().subscribe(data=>console.log(data),
+      e=>console.log(e),
+    ()=>console.log("completed")); //data store in fakeObject 
+    */
+   this.employeeDetails.push(new Employee(100,"Ravi",12000));
+   this.employeeDetails.push(new Employee(101,"Ramesh",14000));
+   this.employeeDetails.push(new Employee(102,"Rajesh",16000));
+   this.fakeSer2.loadData().subscribe(result=>this.fakeObject=result);
   }
+  i=0;
+  id=0;
+  name=null;
+  salary=0.0;
+  loadDataInfo() {
+
+      this.id = this.employeeDetails[this.i].id;
+      this.name = this.employeeDetails[this.i].name;
+      this.salary = this.employeeDetails[this.i].salary;
+      this.i++;
+  }
+  /*
+  this.http.get(url).subscribe(next(),error(),())
+  */
   fun1() {
     let ff1 = new FakeService1();
     this.msg1 = ff1.sayHello();
@@ -26,4 +49,9 @@ export class FakeComponent implements OnInit {
   }
 
 
+}
+
+
+class Employee {
+  constructor(public id:number,public name:string,public salary:number){}  
 }
